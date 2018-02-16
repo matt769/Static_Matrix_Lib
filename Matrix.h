8662700da6483720b3531aae759e593ed8f10bb4
Matt <matthedley@hotmail.co.uk>
2018-02-16 13:24:05 +0000

    all operators added

--- Matrix.h
+++ Matrix.h
@@ -3,15 +3,23 @@
 
 // add setting operationFailure flag
 
-// add flat structure version of the data
+// add flat structure version of the data (can use for some operations)
 
 // change back to bytes
 
 // it's only non-member functions which require template declaration on the same line
 
 // is it inconsistent to have some as member functions and some not?
+// could I move non-member back to member now that it works?
+// or move more to non-member (would this be preferable space-wise?)
 
 // pass by const ref
+
+// operations to add
+// +-/ by scalar
+
+
+
 
 #ifndef MATRIX_H
 #define MATRIX_H
@@ -21,24 +29,20 @@
 template <typename T, uint8_t NUM_ROWS, uint8_t NUM_COLS>
 class Matrix {
   public:
-        bool operationFailure;
+    bool operationFailure;
     T data[NUM_ROWS][NUM_COLS];
     Matrix();
     void zeroInit();
-    //    void identityInit();
-    //    void customInit(T value);
-    //    Matrix copy();
-        Matrix operator+(Matrix m);
-        Matrix operator-(Matrix m);
-    //    Matrix operator*(T scalar);
-    //    Matrix inverse();
-    //    void print();
+    void identityInit();
+    void customInit(T value);
+    Matrix copy();
+    Matrix operator+(Matrix m);
+    Matrix operator-(Matrix m);
+    Matrix operator*(T scalar);
+    Matrix inverse();
+    Matrix<T, NUM_COLS, NUM_ROWS> transpose();
+    void print();
 };
-
-//Matrix operator*(Matrix m);
-//Matrix transpose();
-
-
 
 template <typename T, uint8_t NUM_ROWS, uint8_t NUM_COLS>
 Matrix<T, NUM_ROWS, NUM_COLS>::Matrix() {
@@ -46,13 +50,14 @@ Matrix<T, NUM_ROWS, NUM_COLS>::Matrix() {
   operationFailure = false;
 }
 
-//void Matrix::customInit(float value) {
-//  for (uint8_t8_t i = 0; i < numRows; i++) {
-//    for (uint8_t j = 0; j < numCols; j++) {
-//      data[i][j] = value;
-//    }
-//  }
-//}
+template <typename T, uint8_t NUM_ROWS, uint8_t NUM_COLS>
+void Matrix<T, NUM_ROWS, NUM_COLS>::customInit(T value) {
+  for (uint8_t i = 0; i < NUM_ROWS; i++) {
+    for (uint8_t j = 0; j < NUM_COLS; j++) {
+      data[i][j] = value;
+    }
+  }
+}
 
 template <typename T, uint8_t NUM_ROWS, uint8_t NUM_COLS>
 void Matrix<T, NUM_ROWS, NUM_COLS>::zeroInit() {
@@ -62,33 +67,32 @@ void Matrix<T, NUM_ROWS, NUM_COLS>::zeroInit() {
     }
   }
 }
-//
-//void Matrix::identityInit() {
-//  for (uint8_t i = 0; i < numRows; i++) {
-//    for (uint8_t j = 0; j < numCols; j++) {
-//      if (i == j) {
-//        data[i][j] = 1.0f;
-//      }
-//      else {
-//        data[i][j] = 0.0f;
-//      }
-//    }
-//  }
-//}
-//
-//// returns a copy of the matrix
-//Matrix Matrix::copy() {
-//  Matrix result(numRows, numCols);
-//  for (uint8_t i = 0; i < numRows; i++) {
-//    for (uint8_t j = 0; j < numCols; j++) {
-//      result.data[i][j] = data[i][j];
-//    }
-//  }
-//  return result;
-//}
-//
-//
-//
+
+template <typename T, uint8_t NUM_ROWS, uint8_t NUM_COLS>
+void  Matrix<T, NUM_ROWS, NUM_COLS>::identityInit() {
+  for (uint8_t i = 0; i < NUM_ROWS; i++) {
+    for (uint8_t j = 0; j < NUM_COLS; j++) {
+      if (i == j) {
+        data[i][j] = 1.0f;
+      }
+      else {
+        data[i][j] = 0.0f;
+      }
+    }
+  }
+}
+
+template <typename T, uint8_t NUM_ROWS, uint8_t NUM_COLS>
+Matrix<T, NUM_ROWS, NUM_COLS> Matrix<T, NUM_ROWS, NUM_COLS>::copy() {
+  Matrix<T, NUM_ROWS, NUM_COLS> result;
+  for (uint8_t i = 0; i < NUM_ROWS; i++) {
+    for (uint8_t j = 0; j < NUM_COLS; j++) {
+      result.data[i][j] = data[i][j];
+    }
+  }
+  return result;
+}
+
 template <typename T, uint8_t NUM_ROWS, uint8_t NUM_COLS>
 Matrix<T, NUM_ROWS, NUM_COLS> Matrix<T, NUM_ROWS, NUM_COLS>::operator+(Matrix<T, NUM_ROWS, NUM_COLS> m) {
   Matrix<T, NUM_ROWS, NUM_COLS> result;
@@ -111,6 +115,78 @@ Matrix<T, NUM_ROWS, NUM_COLS> Matrix<T, NUM_ROWS, NUM_COLS>::operator-(Matrix<T,
   return result;
 }
 
+template <typename T, uint8_t NUM_ROWS, uint8_t NUM_COLS>
+Matrix<T, NUM_ROWS, NUM_COLS> Matrix<T, NUM_ROWS, NUM_COLS>::operator*(T scalar) {
+  Matrix<T, NUM_ROWS, NUM_COLS> result;
+  for (uint8_t i = 0; i < NUM_ROWS; i++) {
+    for (uint8_t j = 0; j < NUM_COLS; j++) {
+      result.data[i][j] = data[i][j] * scalar;
+    }
+  }
+  return result;
+}
+
+template <typename T, uint8_t NUM_ROWS, uint8_t NUM_COLS>
+Matrix<T, NUM_COLS, NUM_ROWS> Matrix<T, NUM_ROWS, NUM_COLS>::transpose() {
+  Matrix<T, NUM_COLS, NUM_ROWS> result;
+  for (uint8_t i = 0; i < NUM_ROWS; i++) {
+    for (uint8_t j = 0; j < NUM_COLS; j++) {
+      result.data[j][i] = data[i][j];
+    }
+  }
+  return result;
+}
+
+// add setting of operationFailure flag
+// skip operations that will be on values known to be zero
+// only operates on square matrix
+template <typename T, uint8_t NUM_ROWS, uint8_t NUM_COLS>
+Matrix<T, NUM_ROWS, NUM_COLS> Matrix<T, NUM_ROWS, NUM_COLS>::inverse() {
+  // will only operate on a square matrix
+  // for each column, make diagonal element 1, then make ret of column zero
+
+  Matrix<T, NUM_ROWS, NUM_COLS> result;
+  result.identityInit();
+  Matrix<T, NUM_ROWS, NUM_COLS> temp = copy(); // not going to modify original matrix
+  for (uint8_t pivotPosition = 0; pivotPosition < NUM_ROWS; pivotPosition++) {
+    // want to use the row with the highest absolute value in the column of the current pivot
+    // this can deal with some situations where the diag value is zero
+    // NOT IMPLEMENTING NOW
+
+    // first divide the row by the value at pivot to make pivot = 1
+    float factor = 1.0f / temp.data[pivotPosition][pivotPosition];
+    for (uint8_t j = 0; j < NUM_COLS; j++) {
+      temp.data[pivotPosition][j] *= factor;
+      result.data[pivotPosition][j] *= factor;  // same operation on result matrix
+    }
+    // now for each row k (that isn't the pivot row)
+    for (uint8_t rowToZero = 0; rowToZero < NUM_ROWS; rowToZero++) {
+      if (rowToZero != pivotPosition) {
+        // minus (pivot row * [k][pivotPosition]
+        float rowFactor = temp.data[rowToZero][pivotPosition];
+        for (uint8_t j = 0; j < NUM_COLS; j++) {
+          temp.data[rowToZero][j] -= temp.data[pivotPosition][j] * rowFactor;
+          result.data[rowToZero][j] -= result.data[pivotPosition][j] * rowFactor;  // same operation on result matrix
+        }
+      }
+    }
+  }
+  return result;
+}
+
+
+template <typename T, uint8_t NUM_ROWS, uint8_t NUM_COLS>
+void Matrix<T, NUM_ROWS, NUM_COLS>::print() {
+  for (uint8_t i = 0; i < NUM_ROWS; i++) {
+    for (uint8_t j = 0; j < NUM_COLS; j++) {
+      Serial.print(data[i][j]); Serial.print('\t');
+    }
+    Serial.print('\n');
+  }
+  Serial.print('\n');
+}
+
+
 
 // NOT A MEMBER FUNCTION
 template <typename T, uint8_t ROWS_LEFT, uint8_t SHARED_DIMENSION, uint8_t COLS_RIGHT> Matrix<T, ROWS_LEFT, COLS_RIGHT> operator*(const Matrix<T, ROWS_LEFT, SHARED_DIMENSION> a, const Matrix<T, SHARED_DIMENSION, COLS_RIGHT> b) {
@@ -128,82 +204,5 @@ template <typename T, uint8_t ROWS_LEFT, uint8_t SHARED_DIMENSION, uint8_t COLS_
 }
 
 
-
-//
-//Matrix Matrix::operator*(float scalar) {
-//  Matrix result(numRows, numCols);
-//  for (uint8_t i = 0; i < numRows; i++) {
-//    for (uint8_t j = 0; j < numCols; j++) {
-//      result.data[i][j] = data[i][j] * scalar;
-//    }
-//  }
-//  return result;
-//}
-//
-//
-//void Matrix::print() {
-//  for (uint8_t i = 0; i < numRows; i++) {
-//    for (uint8_t j = 0; j < numCols; j++) {
-//      Serial.print(data[i][j]); Serial.print('\t');
-//    }
-//    Serial.print('\n');
-//  }
-//  Serial.print('\n');
-//}
-//
-//
-//Matrix Matrix::transpose() {
-//  Matrix result(numCols, numRows);
-//  for (uint8_t i = 0; i < numRows; i++) {
-//    for (uint8_t j = 0; j < numCols; j++) {
-//      result.data[j][i] = data[i][j];
-//    }
-//  }
-//  return result;
-//}
-//
-//// add setting of operationFailure flag
-//// skip operations that will be on values known to be zero
-//Matrix Matrix::inverse() {
-//  // will only operate on a square matrix
-//  // for each column, make diagonal element 1, then make ret of column zero
-//
-//  Matrix result(numRows, numCols);
-//  result.identityInit();
-//  Matrix temp = copy(); // not going to modify original matrix
-//  for (uint8_t pivotPosition = 0; pivotPosition < numRows; pivotPosition++) {
-//    // want to use the row with the highest absolute value in the column of the current pivot
-//    // this can deal with some situations where the diag value is zero
-//    // NOT IMPLEMENTING NOW
-//    
-//    // first divide the row by the value at pivot to make pivot = 1
-//    float factor = 1.0f / temp.data[pivotPosition][pivotPosition];
-//    for (uint8_t j = 0; j < numCols; j++) {
-//      temp.data[pivotPosition][j] *= factor;
-//      result.data[pivotPosition][j] *= factor;  // same operation on result matrix
-//    }
-//    // now for each row k (that isn't the pivot row)
-//    for (uint8_t rowToZero = 0; rowToZero < numRows; rowToZero++) {
-//      if (rowToZero != pivotPosition) {
-//        // minus (pivot row * [k][pivotPosition]
-//        float rowFactor = temp.data[rowToZero][pivotPosition];
-//        for (uint8_t j = 0; j < numCols; j++) {
-//          temp.data[rowToZero][j] -= temp.data[pivotPosition][j] * rowFactor;
-//          result.data[rowToZero][j] -= result.data[pivotPosition][j] * rowFactor;  // same operation on result matrix
-//        }
-//      }
-//    }
-//  }
-//  return result;
-//}
-
-
-
-
 #endif
-
-
-
-
-
 
--- Matrix.ino
+++ Matrix.ino
@@ -1,14 +1,30 @@
 #include "Matrix.h"
 
-Matrix<float, 3, 5> A;
-Matrix<float, 3, 5> B;
-Matrix<float, 5, 2> D;
+// test on arduino and teensy
+// check results and performance
+
+
+
+Matrix<float, 3, 4> A;
+Matrix<float, 3, 4> B;
+Matrix<float, 4, 2> D;
 
 void setup() {
-  Matrix<float, 3, 5> C = A + B;
-  Serial.println(C.data[0][0]);
+  Matrix<float, 3, 4> C = A + B;
+  C.print();
   Matrix<float, 3, 2> E = A * D;
-  Serial.println(E.data[0][0]);  
+  E.print();
+  Matrix<float, 3, 4> F = A - B;
+  F.print();
+  Matrix<float, 3, 4> G = A * 1.23f;
+  G.print();
+  Matrix<float, 4, 2> H = D.copy();
+  H.print();
+  Matrix<float, 4, 2> I;
+  I.identityInit();
+  I.inverse().print();  // need to add proper check
+
+  
 }
 
 void loop() {
